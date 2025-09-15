7ab99cb636904984dcba909ecc85501296c070c6
mahaltrupti <truptimahal2004@gmail.com>
2025-09-15 21:26:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+ <script>
+    document.querySelector('input[type="button"]').addEventListener('click', function() {
+      const selectElement = document.getElementById('colorSelect');
+      const selectedOption = selectElement.options[selectElement.selectedIndex];
+      
+      if (selectedOption) {
+        selectElement.removeChild(selectedOption);
+      }
+    });
+  </script>
